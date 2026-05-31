d1314ded064d4a2308246e33f2540edb40bf9094
Owly1988 <masrahul@gmail.com>
2026-05-31 16:19:17 +0200

    init

---
+++ eerste_programma.py
@@ -0,0 +1,23 @@
+print("Hallo")
+print("ik leer Python")
+naam = "Jan"
+leeftijd = 25
+
+print(naam)
+print(leeftijd)
+
+a = 10
+b = 5
+
+print(a + b)
+print(a * b)
+
+naam = input("Wat is je naam? ")
+print("Hallo", naam)
+
+leeftijd = int(input("Hoe oud ben je? "))
+
+if leeftijd >= 18:
+    print("Volwassen")
+else:
+    print("Minderjarig")
